adaf64d57c9a6d385dacaab5beda0a9f396e848b
Lucas Serejo <lucasj.serejo11@gmail.com>
2025-03-16 15:45:12 -0300

    feat: implementar endpoints de gerenciamento de usuários e atualizar o modelo de usuário

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.routes import usuario
+from app.api.routes import usuario 
 
 app = FastAPI()
 
@@ -7,4 +7,4 @@ app.include_router(usuario.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
