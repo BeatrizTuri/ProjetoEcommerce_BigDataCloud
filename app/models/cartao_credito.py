63e6918dcc77ae9aa55368cff0ff96a0b452fb50
PedroRossettoCosta <101264225+PedroRossettoCosta@users.noreply.github.com>
2025-03-28 20:11:14 -0300

    feat/delete cartão com endereço quando deletar usuario

--- app/models/cartao_credito.py
+++ app/models/cartao_credito.py
@@ -1,15 +1,30 @@
 from sqlalchemy import Column, Date, Integer, String, DECIMAL, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, validates
 from app.core.sql_db import Base
 
 class CartaoCredito(Base):
     __tablename__ = "cartao_credito"
     
     id = Column(Integer, primary_key=True, index=True)
-    numero = Column(Integer, nullable=False, unique=True)
+    numero = Column(String(16), nullable=False, unique=True)
     dtExpiracao = Column(Date, nullable=False)
-    cvv = Column(Integer, nullable=False)
+    cvv = Column(String(3), nullable=False)
     saldo = Column(DECIMAL(15, 2), nullable=False)
     id_usuario_cartao = Column(Integer, ForeignKey("usuario.id"), nullable=False)
     
     usuario = relationship("Usuario", back_populates="cartoes")
+
+
+    @validates("numero")
+    def validar_numero(self,key,value):
+        if not value.isdigit() or len(value) != 16:
+            raise ValueError("Numero do cartão só pode ter 16 numeros, o cartão não foi criado")
+        return value
+    
+    @validates("cvv")
+    def validar_cvv(self,key,value):
+        if not value.isdigit() or len(value) != 16:
+            raise ValueError("O cvv só pode ter 3 numeros, o cartão não foi criado")
+    
+    
+    
