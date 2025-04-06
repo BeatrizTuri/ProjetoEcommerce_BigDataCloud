f4681be68b6cafbb307a677322a6e71a202be20d
Lucas Serejo <lucasj.serejo11@gmail.com>
2025-04-06 05:23:26 -0300

    feat: adicionar suporte a SSL na conexão com o banco de dados MySQL

--- app/core/sql_db.py
+++ app/core/sql_db.py
@@ -1,7 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+MYSQL_SSL_CA = os.getenv("MYSQL_SSL_CA")
+
+# Cria a engine com SSL ativado
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl": {"ca": MYSQL_SSL_CA}
+    }
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
