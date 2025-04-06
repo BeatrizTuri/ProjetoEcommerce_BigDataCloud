46416a330367360417a2db309a77f3915f84bf22
Lucas Serejo <lucasj.serejo11@gmail.com>
2025-04-06 14:43:07 -0300

    refactor: comment out SSL connection arguments in engine creation

--- app/core/sql_db.py
+++ app/core/sql_db.py
@@ -8,9 +8,9 @@ MYSQL_SSL_CA = os.getenv("MYSQL_SSL_CA", "certs/DigiCertGlobalRootCA.crt.pem")
 # Cria a engine com SSL ativado
 engine = create_engine(
     DATABASE_URL,
-    connect_args={
-        "ssl": {"ca": MYSQL_SSL_CA}
-    }
+    # connect_args={
+    #     "ssl": {"ca": MYSQL_SSL_CA}
+    # }
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
